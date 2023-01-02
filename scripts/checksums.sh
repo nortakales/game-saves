28f5fd1afa4fbeb83ab551fb591e1e35c5e57046
Nick Ortakales <nortakales@gmail.com>
2023-01-02 00:41:05 -0800

    Added pre-commit hook, removed bad files

--- scripts/checksums.sh
+++ scripts/checksums.sh
@@ -4,18 +4,26 @@
 # for bash, you would need to do: shopt -s globstar
 # This helped: https://thevaluable.dev/zsh-expansion-guide-example/
 
+SCRIPTDIR="$(dirname "$(which "$0")")"
+
+touch EMPTY_FILE.txt
+
 declare -A CHECKSUMS
 
 for FILE in ./**/*(.)
 do
     CHECKSUM=$(md5sum "$FILE" | cut -d' ' -f1)
     # echo "$CHECKSUM $FILE"
-    if [[ ! -z ${CHECKSUMS[$CHECKSUM]} ]]; then
-        echo $FILE " matches " ${CHECKSUMS[$CHECKSUM]}
+    ALLOW_LIST_RESULT=$(grep $FILE $SCRIPTDIR/checksums-allow-list.txt)
+    if [[ ! -z ${CHECKSUMS[$CHECKSUM]} && -z $ALLOW_LIST_RESULT ]]; then
+        echo "\e[0;0m$FILE\e[0;37m matches \e[0;0m${CHECKSUMS[$CHECKSUM]}"
+        echo "\e[0;35m    $CHECKSUM\e[0;0m"
     else
         CHECKSUMS[$CHECKSUM]=$FILE
     fi
 done
+
+rm EMPTY_FILE.txt
 
 # echo "${CHECKSUMS[@]}"
 # echo ${CHECKSUMS['sdf']}
